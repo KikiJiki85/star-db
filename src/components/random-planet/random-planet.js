8d1ad88b304b17b4af9f3d9508d08d86cb058884
Aleksey Vaskin <alzor85@gmail.com>
2021-01-11 15:32:31 +0300

    Adding componentDidMount() to item-list

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -17,11 +17,13 @@ export default class RandomPlanet extends Component {
         error: false,
     };
 
-
-    constructor() {
-        super();
+    componentDidMount() {
         this.updatePlanet();
-        this.interval = setInterval(this.updatePlanet, 3000);
+        this.interval = setInterval(this.updatePlanet, 10000);
+    };
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
     };
 
     onPlanetLoaded = (planet) => {
@@ -39,7 +41,6 @@ export default class RandomPlanet extends Component {
     };
 
     updatePlanet = () => {
-        console.log('update');
         const id = Math.floor(Math.random()* 25 + 2);
         this.swapiService
             .getPlanet(id)
@@ -48,6 +49,7 @@ export default class RandomPlanet extends Component {
     };
 
     render() {
+        console.log('render');
         const {loading, planet, error} = this.state;
 
         const hasData = !(loading || error);
